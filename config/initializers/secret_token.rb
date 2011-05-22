f8edbf123628e803f6bc0ce672c6fe92a5734bfb
fakestarbaby <fakestarbaby@gmail.com>
2011-05-23 07:44:01 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAmazonEcs::Application.config.secret_token = '06926641b77c431980cc4ee0478c431edabf11c45752d3944eafc4fae935b705b24a15b7c3c3e76d9a31371fbcc734a4731a937b207ef3650bb2d6609f53f7b0'
